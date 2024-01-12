271038baf38d27973e6768af2df61f7c34ab4a1f
toad-wart <kurai.jab@gmail.com>
2024-01-12 17:18:15 +1000

    Changes to be committed: 	new file:   comp.sh
     Changes not staged for commit:
    	deleted:    "\320\241\321\202\320\260\320\262\320\270\320\274 Compose"

---
+++ comp.sh
@@ -0,0 +1,7 @@
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list  /dev/null
+apt update
+apt-cache policy docker-ce
+apt install docker-ce docker-compose -y
