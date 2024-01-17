aaa2b033dba4821cd61554b1c8a580e00277c492
toad-wart <kurai.jab@gmail.com>
2024-01-17 13:19:16 +1000

    Changes to be committed:

--- up/up.sh
+++ up/up.sh
@@ -5,6 +5,8 @@ dpkg -i /tmp/zabbix-release_6.0-5+debian12_all.deb
 apt update
 
 apt install zabbix-agent curl nano htop nmap sudo net-tools ntp -y
+
+sed -i "s/127.0.0.1/192.168.10.209/g" /etc/zabbix/zabbix_agentd.conf
 systemctl restart zabbix-agent || systemctl enable zabbix-agent
 
 #ntp up
@@ -13,12 +15,12 @@ systemctl start ntp || service ntp start ||
 cp /usr/share/zoneinfo/Asia/Vladivostok /etc/localtime
 
 #pbis up
-pbis-open-9.1.0.551.linux.x86_64.deb.sh
+sh pbis-open-9.1.0.551.linux.x86_64.deb.sh
 /opt/pbis/bin/domainjoin-cli join --ou OU=LINUX,OU=Srv,DC=SEVEROTORG,DC=LOCAL SEVEROTORG.LOCAL
 /opt/pbis/bin/config RequireMembershipOf "SEVEROTORG.LOCAL\\Linux_administrators"
 /opt/pbis/bin/config UserDomainPrefix SEVEROTORG.LOCAL
 /opt/pbis/bin/config AssumeDefaultDomain true
 /opt/pbis/bin/config LoginShellTemplate /bin/bash
 /opt/pbis/bin/config HomeDirTemplate %H/%U
-
+echo "%SEVEROTORG\\Linux_administrators ALL=NOPASSWD: ALL" >> /etc/sudoers
 
