f515c07e7e987cc62a91cb265c78db7426b99050
Toad-wart <120439575+Toad-wart@users.noreply.github.com>
2024-02-01 13:06:44 +1000

    Update up.sh

--- up/up.sh
+++ up/up.sh
@@ -4,7 +4,7 @@ dpkg -i /tmp/zabbix-release_6.0-5+debian12_all.deb
 
 apt update
 
-apt install zabbix-agent curl nano htop nmap sudo net-tools open-vm-tools-desktop ntp -y
+apt install zabbix-agent curl nano htop nmap sudo net-tools open-vm-tools ntp mc -y
 
 sed -i "s/127.0.0.1/192.168.10.209/g" /etc/zabbix/zabbix_agentd.conf
 sed -i "s/Hostname=/#Hostname=/g" /etc/zabbix/zabbix_agentd.conf
