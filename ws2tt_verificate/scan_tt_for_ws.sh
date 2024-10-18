1f2ba4132809bf5ec5eb7f4e6e1caece6cfd0add
Toad-wart <120439575+Toad-wart@users.noreply.github.com>
2024-10-18 12:06:35 +1000

    Update scan_tt_for_ws.sh

--- ws2tt_verificate/scan_tt_for_ws.sh
+++ ws2tt_verificate/scan_tt_for_ws.sh
@@ -1,13 +1,12 @@
-  GNU nano 4.8                                                                                 scan_tt_for_ws.sh
+  GNU nano 4.8                                                                                ./scan_tt_for_ws.sh
 #!/bin/sh
 
 #Получаем список диапазонов точек из файла
 for tt_ip in $(cat ip_tt)
         do
                 #echo tt_address  $tt_ip
-                tt_name=$(nmap $tt_ip -p 445 | grep "SRV01" | awk '{print $5}' | cut -d'-' -f1) #получили имя сервера
+                tt_name=$(nmap $tt_ip -p 445 | grep -i "srv01" | awk '{print $5}' | cut -d'-' -f1) #получили имя сервера
                 #echo tt_name $tt_name
-                name_err=$(nmap $tt_ip -p 445| grep -Ev $tt_name | grep "\-ws" | awk '{print $5}' | cut -d'.' -f1 ) #получили  имена машин с префиксом не совпадающим с именем сервера в диапазаоне
+                name_err=$(nmap $tt_ip -p 445| grep -iEv $tt_name | grep -i "\-ws" | awk '{print $5}' | cut -d'.' -f1 ) #получили  имена машин с префиксом не совпадающим с именем сервера в диапазаоне
                 echo Список ошибочных имен пк для диапазона $tt_ip и имени ТТ $tt_name: $name_err
 done
-
