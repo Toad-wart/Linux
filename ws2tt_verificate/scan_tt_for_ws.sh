25f77627c1a7ca71f3658e9bee815b31ce590252
Toad-wart <120439575+Toad-wart@users.noreply.github.com>
2024-10-18 11:20:17 +1000

    Update scan_tt_for_ws.sh

--- ws2tt_verificate/scan_tt_for_ws.sh
+++ ws2tt_verificate/scan_tt_for_ws.sh
@@ -5,7 +5,7 @@
 for tt_ip in $(cat ip_tt)
         do
                 #echo tt_address  $tt_ip
-                tt_name=$(nmap $tt_ip -p 445 | grep "srv01" | awk '{print $5}' | cut -d'-' -f1) #получили имя сервера
+                tt_name=$(nmap $tt_ip -p 445 | grep "SRV01" | awk '{print $5}' | cut -d'-' -f1) #получили имя сервера
                 #echo tt_name $tt_name
                 name_err=$(nmap $tt_ip -p 445| grep -Ev $tt_name | grep "\-ws" | awk '{print $5}' | cut -d'.' -f1 ) #получили  имена машин с префиксом не совпадающим с именем сервера в диапазаоне
                 echo Список ошибочных имен пк для диапазона $tt_ip и имени ТТ $tt_name: $name_err
